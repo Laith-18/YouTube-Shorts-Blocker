170cee5c7ea81b54a1916b2fc73f0af2d63e0eff
Laith Hanson <L.Hanson5@newcastle.ac.uk>
2026-02-06 16:19:15 +0000

    Updated content.js after unsuccessful first attempt

--- content.js
+++ content.js
@@ -1,26 +1,61 @@
-if (location.pathname.startsWith("/shorts")) {
-    window.location.replace("/");
-}
+(function () {
+    "use strict";
 
-function removeShorts() {
-    const selectors = [
+    function blockShortsPage() {
+        if (location.pathname.startsWith("/shorts")) {
+            history.replaceState(null, "", "/");
+        }   
+            
+    }
+
+    function removeShorts() {
+        const shortsRenderers = [      
         "ytd-reel-shelf-renderer",
         "ytd-reel-item-renderer",
-        "a[href^='/shorts/']",];
+        "ytd-rich-shelf-renderer",
+        "ytd-shorts",
 
-    selectors.forEach(selector => {
-        document.querySelectorAll(selector).forEach(element => {
-            el.remove();
+        ];
+
+        shortsRenderers.forEach(tag => {
+            document.querySelectorAll(tag).forEach(el => el.remove());
         });
+
+        document.querySelectorAll("a[href^='/shorts']").forEach(link => {
+            const video = link.closest("ytd-grid-video-renderer, ytd-video-renderer, ytd-vertical-list-renderer");
+            if (video) video.remove();
+        });
+    }
+
+    const observer = new MutationObserver(() => {
+        blockShortsPage();
+        removeShorts();
     });
-}
-const observer = new MutationObserver(() => {
-    removeShorts();
-});
 
-observer.observe(document.document, {
-    childList: true,
-    subtree: true
-});
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    })
 
-removeShorts();
+    let lastUrl = location.href;
+    setInterval(() => {
+        if (location.href !== lastUrl) {
+            lastUrl = location.href;
+            blockShortsPage();
+            removeShorts();
+        }
+    }, 500);
+
+// Initial run
+
+    blockShortsPage();
+    removeShorts(); 
+    
+    console.log("YouTube Shorts have been blocked.");
+        
+
+
+
+})();
+
+
